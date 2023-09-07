5e0f4441a550fc54bb08e059d0c57a348cd7b789
adhamessammetwally@gmail.com <adhamessammetwally@gmail.com>
2023-09-07 18:16:48 +0300

    fixes - sed command from task 0 - refactor task 1 to different algorithm

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -13,5 +13,26 @@ echo "<html>
 </html>" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -hR ubuntu:ubuntu /data/
-sudo sed -i '38i\\tlocation /hbtn_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+printf %s "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By $HOSTNAME;
+    root   /var/www/html;
+    index  index.html index.htm;
+
+    location /hbnb_static {
+	alias /data/web_static/current;
+	index index.html index.htm;
+    }
+
+    location /redirect_me {
+	return 301 http://cuberule.com/;
+    }
+
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}" > /etc/nginx/sites-available/default
 sudo service nginx start
