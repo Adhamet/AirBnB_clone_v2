07a8cbb56b0aa70b1abd09996f75256760d3e5a5
adhamessammetwally@gmail.com <adhamessammetwally@gmail.com>
2023-09-23 08:26:44 +0300

    py script that starts flask web-app

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Starts flask web application with specific requirements"""
+from flask import Flask
+
+app = Flask(__name__)
+
+# Define Route
+@app.route('/', strict_slashes=False)
+def home():
+    return 'Hello HBNB!'
+
+if __name__ == '__main__':
+    """run flask web-app"""
+    app.run(host='0.0.0.0', port=5000, debug=True)
