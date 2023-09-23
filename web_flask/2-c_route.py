f0e43a6c88c6d2faf8ac0383579205ef4e006cb1
adhamessammetwally@gmail.com <adhamessammetwally@gmail.com>
2023-09-23 08:39:41 +0300

    C-route added

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Starts flask web application with specific requirements"""
+from flask import Flask, escape
+
+app = Flask(__name__)
+
+
+# Define Hello-HBNB-Route
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """prints Hello HBNB"""
+    return 'Hello HBNB!'
+
+
+# Define HBNB-Route
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """prints HBNB"""
+    return 'HBNB'
+
+
+# Define C-Route
+@app.route('/c/<text>', strict_slashes=False)
+def custom_text(text):
+    text = escape(text).replace('_', ' ')
+    return 'C {}'.format(text)
+
+
+if __name__ == '__main__':
+    """run flask web-app"""
+    app.run(host='0.0.0.0', port=5000)
